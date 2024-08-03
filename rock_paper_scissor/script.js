04723cd13d8293b45381a9bca3cfba9055b7659d
oniids <jdino.abaya@gmail.com>
2024-08-03 13:49:22 +0800

    modify reset feature

--- rock_paper_scissor/script.js
+++ rock_paper_scissor/script.js
@@ -11,11 +11,9 @@ function getComputerChoice(){
             return "✌️";
             break;
     }
-}
-
-
-
-function playRound(playerChoice, computerChoice){
+ }
+ 
+ function playRound(playerChoice, computerChoice){
     if (playerChoice === computerChoice ){
         result.style.color = "blue";
         result.textContent = "Draw! Try Again.";
@@ -46,13 +44,14 @@ function playRound(playerChoice, computerChoice){
             result.style.color = "red"
             result.textContent = "Computer wins! Scissors beats Paper";
         }
-        
+       
     }
     container.append(score)
     score.textContent = `Player Score: ${playerScore} --- Computer Score: ${computerScore} `
-}
-
-function playGame(player_choice){
+ }
+ 
+ 
+ function playGame(player_choice){
     switch(player_choice){
         case 0:
             player_choice = "✊";
@@ -64,55 +63,86 @@ function playGame(player_choice){
             player_choice = "✌️";
             break;
     }
-
+ 
+ 
     const playerSelection = player_choice;
     const computerSelection = getComputerChoice();
-
-
+ 
     body.appendChild(div);
     div.appendChild(player);
     div.appendChild(computer);
     div.appendChild(result);
     player.textContent = `Player Choice: ${playerSelection.toUpperCase()}`;
     computer.textContent = `Computer Choice: ${computerSelection.toUpperCase()}`;
-    
-
-    playRound(playerSelection, computerSelection);
-}
-
-
-
-let humanScore = 0;
-let computerScore = 0;
-let game_on = true;
-
-
-const buttons = document.querySelectorAll("button");
-let player_choice;
-
-const container = document.querySelector('.container')
-const div = document.createElement('div');
-div.classList = "board";
-const body = document.body;
-const player = document.createElement("h2");
-const computer = document.createElement("h2");
-const result = document.createElement("h2");
-const score = document.createElement("div");
-score.classList = "score"
-const resetButton = document.createElement("button");
-resetButton.className = "reset";
-resetButton.textContent = "Play Again";
-
-
-let isGameOngoing = true;
-
-for (let i = 0; i<buttons.length; i++){
-    buttons[i].addEventListener('click', () =>{
-        if (buttons[i].click){
-            playGame(i);
-            
-        }
-    })
-}
    
+    playRound(playerSelection, computerSelection);
+ }
+ 
 
+ function resetGame() {
+    player.textContent = "";
+    computer.textContent = "";
+    result.textContent = "";
+    playerScore = 0;
+    computerScore = 0;
+    isGameOngoing = true;
+    score.textContent = `Player Score: ${playerScore} --- Computer Score: ${computerScore} `
+    resetButton.style.display ="none";
+ }
+ 
+ 
+ 
+ 
+ let playerScore = 0;
+ let computerScore = 0;
+ 
+ const buttons = document.querySelectorAll("button");
+ let player_choice;
+ 
+ const container = document.querySelector('.container')
+ const div = document.createElement('div');
+ div.classList = "board";
+ const body = document.body;
+ const player = document.createElement("h2");
+ const computer = document.createElement("h2");
+ const result = document.createElement("h2");
+ const score = document.createElement("div");
+ score.classList = "score"
+ const resetButton = document.createElement("button");
+ resetButton.className = "reset";
+ resetButton.textContent = "Play Again";
+ 
+ let isGameOngoing = true;
+ 
+ 
+ for (let i = 0; i<buttons.length; i++){
+    buttons[i].addEventListener('click', () =>{
+        if (isGameOngoing){
+            if (buttons[i].click){
+                player.textContent = "";
+                computer.textContent = "";
+                result.textContent = ""
+                playGame(i);
+                if (playerScore === 5) {
+                    alert("Player Wins!");
+                    isGameOngoing = false;
+                    div.appendChild(resetButton);
+                    resetButton.style.display = "block";
+                    resetButton.addEventListener("click", resetGame);
+                } else if (computerScore === 5){
+                    alert("Computer Wins!");
+                    isGameOngoing = false;
+                    div.appendChild(resetButton);
+                    resetButton.style.display = "block";
+                    resetButton.addEventListener("click", resetGame);
+                }
+            }
+    }
+    })
+ }
+ 
+ 
+ 
+ 
+ 
+ 
